90f7acd15fe9f895e86c21d75a6dde04449989f7
danielreis1 <ddreis88@gmail.com.com>
2017-12-02 22:28:31 +0000

    ex 1 & more general funcs

--- 1.py
+++ 1.py
@@ -1,6 +1,5 @@
 from functions import *
 
-
 def read_input():
     all_docs = []
     f = open('exercise1.txt', 'r')
@@ -9,22 +8,10 @@ def read_input():
 
     return all_docs
 
-nltk.download('punkt')
 graph = {} # grafo com edges igual a cosine similarities
-'''
-the graph is created by the following kind of element:
-where Di is document i
-id is index of 
-each graph element is:
-    key: id of element Di, value: set that contains the documents that link to document Di
-    document links only if cosine_sim between them is higher than a certain threshold
-'''
 
-nltk.download('stopwords')
-stopwords = nltk.corpus.stopwords.words('english')
 all_docs = [] #lista com os documentos todos
 all_docs_sentences = [] #lista com lista de frases de cada doc
-
 
 all_docs = read_input()
 all_docs_sentences = read_documents_into_sentence_tokens(all_docs)
@@ -39,11 +26,9 @@ print (all_docs_sentences)
 print('')
 '''
 
+vectorizer = TfidfVectorizer(norm='l2', min_df=0, use_idf=True, smooth_idf=False, sublinear_tf=True,stop_words=stopwords)
+vectorizer.fit(all_docs)
+graph = createGraph(all_docs_sentences, 0.2, vectorizer)
 
-vectorizer = TfidfVectorizer(norm='l2', min_df=0, use_idf=True, smooth_idf=False, sublinear_tf=True, stop_words = stopwords)
-
-x = vectorizer.fit_transform(all_docs_sentences)
-
-for i in all_docs_sentences:
-    x2 = vectorizer.transform(i)
+print (graph)
 
--- functions.py
+++ functions.py
@@ -5,6 +5,10 @@ from sklearn.metrics.pairwise import linear_kernel
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
+nltk.download('punkt')
+nltk.download('stopwords')
+# global vars
+stopwords = nltk.corpus.stopwords.words('english')
 
 def read_doc_file():
     '''
@@ -29,36 +33,63 @@ def read_documents_into_sentence_tokens(all_docs):
         for i in range(len(sentence_tokens)):
             sentence_tokens[i] = sentence_tokens[i].replace('\n', '')
             sentence_tokens[i] = sentence_tokens[i].replace('\r', '')
+            sentence_tokens[i] = sentence_tokens[i].replace('\t', '')
         sentence_tokens = sentence_tokens[:-1]
         all_docs_sentences.append(sentence_tokens)
     return all_docs_sentences
 
-def cosine_similarities(x, x2, thresholdCS = 0):
+def cos_sims(x, x2, thresholdCS=0):
     '''
     :param x: sparseMatrix with all sentences transform
     :param x2: sparseM with 1 sentence transform
     :param thresholdCS: if value is provided returns only cosine_sims above the given threshold value
     :return: returns cosine_sims, all if no thresholdCS argument is provided
+    filters cosine_sims list
     '''
-    cosine_sims = cosine_similarity(x,x2)
+
+    conected = []
+    cosine_sims = cosine_similarity(x, x2)
     for i in range(len(cosine_sims)):
-        if i < thresholdCS:
-            del cosine_sims[i]
-    return cosine_sims
+        if cosine_sims[i] < thresholdCS:
+            conected.append(i) # apends index of the sentence in the sentence list
+    return conected
 
-def addToGraph(graph, element, thresholdCS):
+def addToGraph(graph, id, sim_indexes):
     '''
-    args:
-        graph where the element is added
-        element to be added
-        thresholdCS threshold cosine similarity
+    :param graph:
+    :param id:
+    :param sim_indexes:
+    :return:
+
     adds an element to the graph
-    when an element X is added the cosine similarity between it and all other elements is computed (elements yi),
-    if the value of the cosine similarity is higher than the thresholdCS, then X is connected to Yi
-    if element similarity is not above threshold with any element, it's dropped and not added to the graph
     '''
 
-
+    graph[str(id)] = sim_indexes
     return
 
 
+def createGraph(elements, thresholdCS, vectorizer):
+    '''
+    :param elements: sentences in all docs
+    :param thresholdCS:
+    :param vectorizer: vectorizer with fitted vocabulary
+    :return: graph
+    '''
+    '''
+    the graph is created by the following kind of element:
+    where Si is sentence i
+    id is index of the sentence in all_docs_sentences
+
+    each graph element is:
+        key: id of element Si, value: set that contains the sentences that link to sentence Si
+        sentences link only if cosine_sim between them is higher than a certain threshold
+    '''
+
+    graph = {}
+    for sentences in elements: # sentences is senteces in a document
+        x = vectorizer.transform(sentences)
+        for i in range(len(sentences)):
+            x2 = vectorizer.transform([sentences[i]])
+            indexes = cos_sims(x, x2, thresholdCS)
+            addToGraph(graph, i, indexes)
+    return graph
