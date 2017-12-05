4693c01a9df325f973f0f19d03b9d2779e667f98
danielreis1 <ddreis88@gmail.com.com>
2017-12-05 03:08:58 +0000

    read doc file functions.py bug fix, now reads title as line

--- 2.py
+++ 2.py
@@ -1,10 +1,13 @@
 from functions import *
 
 all_docs = read_doc_file('TextoFonteComTitulo')
-all_sentences = read_documents_into_sentence_tokens(all_docs)
+all_sentences = read_documents_into_sentence_tokens([all_docs[0]])
 
-#print (all_sentences)
+print (all_sentences)
 
 # dicionario com as varias funcoes, depois for entre todas elas e representar cada uma diferente, {'nome_func': func}
+
+vectorizer = TfidfVectorizer(norm='l2', min_df=0, use_idf=True, smooth_idf=False, sublinear_tf=True, stop_words=stopwords)
+vectorizer.fit(all_docs)
 
 
--- functions.py
+++ functions.py
@@ -19,7 +19,8 @@ def read_doc_file(path):
     all_files = os.listdir(path)
     for fl in all_files:
         f = open(path + '/' + fl, 'r')
-        content = f.read()
+        content = f.readline().decode('cp1252') + '.'  # primeira frase e' o titulo
+        content += f.read().decode('cp1252')
         all_docs.append(content)
     return all_docs
 
@@ -52,27 +53,28 @@ def cos_sims(x, x2, thresholdCS=0):
     :param x: sparseMatrix with all sentences transform
     :param x2: sparseM with 1 sentence transform
     :param thresholdCS: if value is provided returns only cosine_sims above the given threshold value
-    :return: returns cosine_sims, all if no thresholdCS argument is provided
+    :return: returns list with indexes of the phrases with a cosine similarity above threshold
     filters cosine_sims list
     '''
 
     conected = []
     cosine_sims = cosine_similarity(x, x2)
     for i in range(len(cosine_sims)):
-        if cosine_sims[i] < thresholdCS:
+        if cosine_sims[i] > thresholdCS:
             conected.append(i) # apends index of the sentence in the sentence list
     return conected
 
-def addToGraph(graph, id, sim_indexes):
+def addToGraph(graph, id, vals):
     '''
     :param graph:
     :param id: index da frase na lista de frases
-    :param sim_indexes: indexes na lista de frases cujas frases ultrapassam a threshold de semelhanca
+    :param vals: valores do grafo, pode ser uma lista com features ou uma feature so:
+      ex: indexes na lista de frases cujas frases ultrapassam a threshold de semelhanca
     :return:
     adds an element to the graph
     '''
 
-    graph[str(id)] = sim_indexes
+    graph[str(id)] = vals
     return
 
 def createGraph(elements, thresholdCS, vectorizer):
@@ -116,10 +118,10 @@ def PR_sentences_pos(doc_senteces):
     ret = []
     N = len(doc_senteces)
     for i in range(len(doc_senteces)):
-        ret[str(i)] = N-i
+        ret[str(i)] = 1/N
     return  ret
 
-def PR_EW_TFIDF(x,x2):
+def PR_EW_TFIDF(vectorizer):
     '''
     :param x: vectorizer transform for element 1 
     :param x2: same as x for element 2
@@ -128,11 +130,16 @@ def PR_EW_TFIDF(x,x2):
     '''
     works for edges and prior
     '''
-    
-    
-    return
+
+    x = vectorizer.transform()
+    x2 = vectorizer.transform()
+
+
+    return cosine_similarity(x, x2)
 
 def PR_prob_NaiveBayes():
+
+
     return
 
 def EW_nounPhrases():
@@ -146,6 +153,8 @@ def EW_SVD():
     '''
         
     '''
+
+
     return
 
 
