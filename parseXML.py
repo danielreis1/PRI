8aa3b48bef77107128d5630fbdde401059877aac
Joana Chumbo <joana.chumbo@tecnico.ulisboa.pt>
2017-12-06 12:44:13 +0000

    parse 2.0

--- parseXML.py
+++ parseXML.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 import feedparser
+import re
 import xml.etree.ElementTree as ET
 
 sites =	["http://www.nytimes.com/services/xml/rss/nyt/World.xml",\
@@ -13,23 +14,25 @@ sites =	["http://www.nytimes.com/services/xml/rss/nyt/World.xml",\
 		#Apaguei 'blog atwar' do NYT porque dava erro (moved permanently)
 
 d = {}
+clean_tags = re.compile(r'<[^>]+>')	# regular expression to clean HTML tags from text
+
+def remove_tags(text):
+	return clean_tags.sub('', text)
 
 for site in sites:
 	print('\n\t***\t' + site + '\t***\t')
 	d[site] = feedparser.parse(site)
-	entries_keys = d[site]['entries'][0].keys()	# [0] para tirar da lista; interessa-nos as entries 
-	print(entries_keys)
+	entries = d[site]['entries']
 
-'''
-resultado do print(entries_keys) para cada site: (igual para todos)
+	for entry in entries:
+		#links = {site : entry['link']}
+		titles_source = {entry['link'] : entry['title']}
+		titles = list(titles_source.values())[0]
+		summary = remove_tags(re.sub('\.{3}', '', entry['summary'])) # tira tags HTML e reticencias
+		summaries_source = {entry['link'] : summary}
+		summaries = list(summaries_source.values())[0]
+		print('\n***\tTitle\t***')
+		print(titles)
+		print('\n***\tSummary\t***')
+		print(summaries)
 
-dict_keys(['media_content', 'guidislink', 'title_detail', 'link', 'tags', 'author_detail', 'content', 'author', 'authors', 'title', 'published_parsed', 'summary', 'published', 'links', 'credit', 'id', 'summary_detail', 'media_credit'])
-'''
-
-# e para tirar 'content'
-
-'''
-with urlopen("https://sunlightlabs.github.io/congress/legislators?api_key='(myapikey)") as conn:
-    # dosomething
-    a = 0
-'''
