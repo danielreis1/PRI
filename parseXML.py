2a135708c576af851b16008c4c6a85f0c3194da8
danielreis1 <ddreis88@gmail.com.com>
2017-12-06 18:22:54 +0000

    parse fix

--- parseXML.py
+++ parseXML.py
@@ -1,4 +1,3 @@
-from urllib.request import urlopen
 import feedparser
 import re
 import xml.etree.ElementTree as ET
@@ -11,28 +10,24 @@ sites =	["http://www.nytimes.com/services/xml/rss/nyt/World.xml",\
 		"http://www.latimes.com/world/africa/rss2.0.xml", "http://www.latimes.com/world/mexico-americas/rss2.0.xml",\
 		"http://www.latimes.com/world/asia/rss2.0.xml", "http://www.latimes.com/world/europe/rss2.0.xml", "http://www.latimes.com/world/middleeast/rss2.0.xml",\
 		"http://www.latimes.com/world/worldnow/rss2.0.xml"]
-		#Apaguei 'blog atwar' do NYT porque dava erro (moved permanently)
 
 d = {}
 clean_tags = re.compile(r'<[^>]+>')	# regular expression to clean HTML tags from text
 
 def remove_tags(text):
-	return clean_tags.sub('', text)
+    return clean_tags.sub('', text)
+
 
 for site in sites:
-	print('\n\t***\t' + site + '\t***\t')
-	d[site] = feedparser.parse(site)
-	entries = d[site]['entries']
+    #print('\n\t***\t' + site + '\t***\t')
+    d[site] = feedparser.parse(site)
+    entries = d[site]['entries']
+    #print entries
+    #print
+    for entry in entries:
+        #links = {site : entry['link']}
+        texts = {entry['title'] : entry['content'][0]['value']}
+        print texts
 
-	for entry in entries:
-		#links = {site : entry['link']}
-		titles_source = {entry['link'] : entry['title']}
-		titles = list(titles_source.values())[0]
-		summary = remove_tags(re.sub('\.{3}', '', entry['summary'])) # tira tags HTML e reticencias
-		summaries_source = {entry['link'] : summary}
-		summaries = list(summaries_source.values())[0]
-		print('\n***\tTitle\t***')
-		print(titles)
-		print('\n***\tSummary\t***')
-		print(summaries)
+
 
